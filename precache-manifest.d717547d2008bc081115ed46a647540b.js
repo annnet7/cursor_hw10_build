9e7656830055c55807c55601ee405947e9ee337f
annnet7 <annnet7@gmail.com>
2019-09-02 14:36:55 +0300

    first commit

---
+++ precache-manifest.d717547d2008bc081115ed46a647540b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a03c161ab91b05766f6345e3ffa131",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ae1eb9e91da12acca0f",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9bc31012fe1bc7d1ec7a",
+    "url": "/static/js/2.fed19712.chunk.js"
+  },
+  {
+    "revision": "5ae1eb9e91da12acca0f",
+    "url": "/static/js/main.2f8ad320.chunk.js"
+  },
+  {
+    "revision": "8591814d9adce2f0138c",
+    "url": "/static/js/runtime~main.43721f5f.js"
+  }
+]);
